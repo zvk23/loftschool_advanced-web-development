0d5514481e954fd15681c4ce5ecb62a8ac990569
Efimenko <zvikktor@yandex.ru>
2019-03-16 20:00:10 +0300

    random value for skills

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,13 +1,19 @@
 export default (function changeHeader() {
 	let header = document.querySelector(".header");
 
+	if (window.scrollY > 100) {
+		header.classList.add("is-fixed");
+	} else {
+		header.classList.remove("is-fixed");
+	}
+
 	window.addEventListener("wheel", e => {
 		let offset = window.scrollY;
 
-        if (offset > 100) {
-            header.classList.add('is-fixed');
-        } else {
-            header.classList.remove('is-fixed');
-        }
+		if (offset > 100) {
+			header.classList.add("is-fixed");
+		} else {
+			header.classList.remove("is-fixed");
+		}
 	});
 })();
